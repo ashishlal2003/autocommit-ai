8cdbc6b1cb6c84a6a11009ac982c0ef4f0e1ad32
Ashish Lal <ashishlal2003@gmail.com>
2023-12-28 20:48:14 +0530

    chenges

--- geminiLLM.js
+++ geminiLLM.js
@@ -1,7 +1,6 @@
 const { GoogleGenerativeAI } = require("@google/generative-ai");
 // require('dotenv').config();
 
-// // Access your API key as an environment variable (see "Set up your API key" above)
 const genAI = new GoogleGenerativeAI("AIzaSyD3I20VtW3E9Bpd56GCOO8WS0Z4MMf-JWE");
 async function run() {
   // For text-only input, use the gemini-pro model
@@ -21,3 +20,5 @@ async function run() {
 module.exports = {
     run
 };
+
+// a new comment added to test git changes
